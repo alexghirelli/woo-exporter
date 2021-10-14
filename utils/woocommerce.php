37544418481f8b9978eb83e07f7f6456a8d125ea
Alex Ghirelli <info@alexghirelli.it>
2021-10-14 18:50:14 +0200

    Utility functions for fetch data from WooCommerce

---
+++ utils/woocommerce.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * WooCommerce utils functions
+ *
+ * @link       https://www.alexghirelli.it
+ * @since      1.0.0
+ *
+ * @package    Woo_Exporter
+ * @subpackage Woo_Exporter/includes
+ */
+
+class WooCommerceUtils {
+
+	/**
+	 * Get Order data
+	 *
+	 * Get all data of a specific order given its order ID
+	 *
+	 * @since    1.0.0
+	 */
+	public function getOrder($orderId) {
+        $order = wc_get_order( $orderId );
+
+        $order_data = $order->get_data();
+        $order_data['date'] = date("Y-m-d", strtotime($order->get_date_created()));
+
+        return $order_data;
+    }
+
+    /**
+	 * Get all Orders data
+	 *
+	 * Get all data of orders given start and end date
+	 *
+	 * @since    1.0.0
+	 */
+	public function getOrders($startDate, $endDate) {
+        // TODO: implement get all orders data
+    }
+
+}
